0a9b674e4ea56a018c0f0ad06b8ac4c2909bae3d
havishat <sundariha@gmail.com>
2017-08-15 12:42:00 -0700

    add

--- getting_started/main/apps/first_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views           # This line is new!
-
-urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^test$', views.test)    # This line has changed!
-]
-
--- multipleapps/apps/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views           # This line is new!
-urlpatterns = [
-    url(r'^$', views.index),     # This line has changed!
-    url(r'^register$', views.register),
-    url(r'^login$', views.login),
-    url(r'^new$', views.register),
-]
